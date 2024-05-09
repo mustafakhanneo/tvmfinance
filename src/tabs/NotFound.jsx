6c46bebb1e6e7fed723d3a35681ce6e7fac884f1
Mustafa Khan <mustafakhanneo@gmail.com>
2024-05-09 13:03:14 +0500

    add site navigation and react router

---
+++ src/tabs/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+        <p>Your desire page is not exist in reality</p>
+        <p>But present in our list and we are working on it to make it <span>Reality</span></p>
+    </div>
+  )
+}
+
+export default NotFound
