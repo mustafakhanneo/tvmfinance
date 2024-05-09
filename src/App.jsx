30212aabf7d0080b9eeaac651b62ab651e6b70ca
Mustafa Khan <mustafakhanneo@gmail.com>
2024-05-09 15:03:54 +0500

    add custom font KOLA

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className='space-y-16 p-[1rem] m-auto max-w-[1280px]'>
     <div className="navbar items-center justify-center rounded-lg bg-base-200">
   <div className="navbar-center">
-    <a className="btn btn-ghost font-bold text-xl" href='./'>Corporate Finance: Time Value of Money</a>
+    <a className="btn btn-ghost font-kola text-xl" href='./'>Corporate Finance: Time Value of Money</a>
   </div>
   
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        kola : ["Kola", "sens-serif"]
+      }
+    },
   },
   plugins: [
     require('daisyui'),
