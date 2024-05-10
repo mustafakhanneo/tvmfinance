a07f05a1ea349df998b724418eb7713fb3ac8c48
Mustafa Khan <mustafakhanneo@gmail.com>
2024-05-10 20:46:54 +0500

    add annuity ordinary and due formulas

--- src/tabs/Formula.jsx
+++ src/tabs/Formula.jsx
@@ -3,7 +3,12 @@ import KatexSpan from '../comps/KatexSpan'
 
 function Formula() {
  
-  const simpleAnnuity = `$ FV = PV$ x $\\left(1 + r\\right)^n$`;
+  const simpleAnnuity = `$ FV = PV \\times \\left(1 + i\\right)^n$`;
+  const futureOdiAnnuity = `$ FV = C \\times \\biggl[\\dfrac{\\left(1 + i\\right)^n - 1}{i}\\biggr]$`;
+  const presentOdiAnnuity = `$ PV = C \\times \\biggl[\\dfrac{1- \\left(1 + i\\right)^{-n}}{i}\\biggr]$`;
+  const futureDueAnnuity = `$ FV = C \\times \\biggl[\\dfrac{\\left(1 + i\\right)^n -1}{i}\\biggr] \\times \\left(1 + i\\right)$`;
+  const presentDueAnnuity = `$ PV = C \\times \\biggl[\\dfrac{1- \\left(1 + i\\right)^{-n}}{i}\\biggr] \\times \\left(1 + i\\right)$`;
+
 
   return (
     <div className='space-y-6 p-[1rem] m-auto max-w-[1280px]'>
@@ -23,23 +28,45 @@ function Formula() {
   <div className='flex flex-col space-y-2 pl-5 pt-2'>
   <KatexSpan text={`$FV =$ Future Value`} />
   <KatexSpan text={`$PV =$ Present Value`} />
-  <KatexSpan text={`$r =$ Interest Rate`} />
+  <KatexSpan text={`$i =$ Interest Rate`} />
   <KatexSpan text={`$n =$ Period`} />
   </div>
   </div>
       </details>
 {/* Ordinary Annuity Calculator  */}
       <div className='space-y-6'>
-        {/* Present Value of Ordinary Annuity */}
+        {/* Future Value of Ordinary Annuity */}
         <details className="collapse collapse-arrow bg-base-200">
   <summary className="collapse-title sm:text-xl text-md font-medium">Future Value of Ordinary Annuity</summary>
-  
+  <div className='flex flex-col justify-center pb-5'>
+    <div className='flex justify-center items-center'>
+  <KatexSpan text={futureOdiAnnuity} />
+  </div>
+  <p className='flex pl-5 font-medium'>Where:</p>
+  <div className='flex flex-col space-y-2 pl-5 pt-2'>
+  <KatexSpan text={`$FV =$ Future Value`} />
+  <KatexSpan text={`$C =$ Cash Flow`} />
+  <KatexSpan text={`$i =$ Interest Rate`} />
+  <KatexSpan text={`$n =$ Period`} />
+  </div>
+  </div>
 </details>
 
         {/* Present Value of Ordinary Annuity */}
         <details className="collapse collapse-arrow bg-base-200">
   <summary className="collapse-title sm:text-xl text-md font-medium">Present Value of Ordinary Annuity</summary>
-  
+  <div className='flex flex-col justify-center pb-5'>
+    <div className='flex justify-center items-center'>
+  <KatexSpan text={presentOdiAnnuity} />
+  </div>
+  <p className='flex pl-5 font-medium'>Where:</p>
+  <div className='flex flex-col space-y-2 pl-5 pt-2'>
+  <KatexSpan text={`$PV =$ Present Value`} />
+  <KatexSpan text={`$C =$ Cash Flow`} />
+  <KatexSpan text={`$i =$ Interest Rate`} />
+  <KatexSpan text={`$n =$ Period`} />
+  </div>
+  </div>
         </details>
       </div>
       {/* Simple Annuity Calculator  */}
@@ -49,13 +76,35 @@ function Formula() {
         {/* Present Value of Annuity Due */}
         <details className="collapse collapse-arrow bg-base-200">
   <summary className="collapse-title sm:text-xl text-md font-medium">Future Value of Annuity Due</summary>
-  
+  <div className='flex flex-col justify-center pb-5'>
+    <div className='flex justify-center items-center'>
+  <KatexSpan text={futureDueAnnuity} />
+  </div>
+  <p className='flex pl-5 font-medium'>Where:</p>
+  <div className='flex flex-col space-y-2 pl-5 pt-2'>
+  <KatexSpan text={`$FV =$ Future Value`} />
+  <KatexSpan text={`$C =$ Cash Flow`} />
+  <KatexSpan text={`$i =$ Interest Rate`} />
+  <KatexSpan text={`$n =$ Period`} />
+  </div>
+  </div>
         </details>
 
         {/* Present Value of Annuity DUe */}
         <details className="collapse collapse-arrow bg-base-200">
   <summary className="collapse-title sm:text-xl text-md font-medium">Present Value of Annuity Due</summary>
-  
+  <div className='flex flex-col justify-center pb-5'>
+    <div className='flex justify-center items-center'>
+  <KatexSpan text={presentDueAnnuity} />
+  </div>
+  <p className='flex pl-5 font-medium'>Where:</p>
+  <div className='flex flex-col space-y-2 pl-5 pt-2'>
+  <KatexSpan text={`$PV =$ Present Value`} />
+  <KatexSpan text={`$C =$ Cash Flow`} />
+  <KatexSpan text={`$i =$ Interest Rate`} />
+  <KatexSpan text={`$n =$ Period`} />
+  </div>
+  </div>
 </details>
 
 {/* Present Value of Growing Annuity */}
