e518d1d23bd288ff772be2fb249577268bf4f1ef
Mustafa Khan <mustafakhanneo@gmail.com>
2024-05-12 16:03:13 +0500

    update about

--- src/tabs/About.jsx
+++ src/tabs/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div className='flex flex-col space-y-4'>
     <div className='flex flex-col justify-start space-y-2'>
       <p className='font-medium font-tanker text-2xl mb-2'>Developer Profile</p>
-      <p className='font-medium text-lg font-archivo'>I'm <span className='font-tanker text-yellow-500'>Mustafa Khan</span>, a developer with a strong background in Economics and Finance from NED University. My passion for coding complements my expertise, allowing me to create tailored solutions for finance websites. Proficient in React, NextJS, Dart, React Native, JavaScript, and SQL, I specialize in building intuitive interfaces, secure payment gateways, and advanced analytics tools</p>
+      <p className='font-medium text-lg font-archivo'>I'm <span className='font-tanker text-yellow-500'>Mustafa Khan</span>, a developer with a strong background in Economics and Finance from NED University. My passion for coding complements my expertise, allowing me to create tailored solutions for finance websites. Proficient in React, NextJS, Dart, React Native, JavaScript, and SQL.</p>
       <p className='font-medium text-lg font-archivo'>With a passion for continuous learning and staying updated with the latest developments in both finance and technology, I am dedicated to staying at the forefront of innovation and driving success for your finance website. Let's collaborate to build a digital platform that empowers users to make informed financial decisions and achieve their goals with confidence.</p>
     </div>
     <div className='grid sm:grid-cols-6 grid-cols-1 gap-4'>
